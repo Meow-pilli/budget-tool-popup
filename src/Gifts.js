46464510041a380a73e42f635314b85c52baa956
pavit <pavit2000@gmail.com>
2024-12-04 11:03:30 -0700

    Updated changes

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,9 +1,25 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Page.css";
+import "./Gifts.css";
+
+const data = [
+    { item: "Family", budget: 500.0, spent: 495.0, difference: 5.0 },
+    { item: "Friends", budget: 250.0, spent: 325.0, difference: -75.0 },
+    { item: "Co-workers", budget: 0.0, spent: 0.0, difference: 0.0 },
+    { item: "Teachers, nannies, babysitters, etc.", budget: 0.0, spent: 0.0, difference: 0.0 },
+    { item: "Charitable donations", budget: 0.0, spent: 0.0, difference: 0.0 },
+    { item: "Other (tab in last column of this row to add row)", budget: 0.0, spent: 0.0, difference: 0.0 },
+  ];
+  
+function calculateTotal(data, key) {
+    return data.reduce((acc, row) => acc + row[key], 0).toFixed(2);
+}
 
 function Gifts() {
   const navigate = useNavigate();
+  const totalBudget = calculateTotal(data, "budget");
+  const totalSpent = calculateTotal(data, "spent");
+  const totalDifference = calculateTotal(data, "difference");
 
   useEffect(() => {
     // Hide the global menu when this component is rendered
@@ -32,7 +48,48 @@ function Gifts() {
         </div>
       </header>
       <div className="gifts-body">
-        <p></p>
+        <p>
+            <table>
+                <thead>
+                <tr>
+                    <th>ITEM</th>
+                    <th>BUDGET</th>
+                    <th>SPENT</th>
+                    <th>DIFFERENCE</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((row, index) => (
+                    <tr key={index}>
+                    <td>{row.item}</td>
+                    <td>${row.budget.toFixed(2)}</td>
+                    <td>${row.spent.toFixed(2)}</td>
+                    <td>
+                        {row.difference >= 0 ? (
+                        <span className="checkmark">✔ ${row.difference.toFixed(2)}</span>
+                        ) : (
+                        <span className="cross">✘ ${row.difference.toFixed(2)}</span>
+                        )}
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td>Total</td>
+                    <td>${totalBudget}</td>
+                    <td>${totalSpent}</td>
+                    <td>
+                    {totalDifference >= 0 ? (
+                        <span className="checkmark">✔ ${totalDifference}</span>
+                    ) : (
+                        <span className="cross">✘ ${totalDifference}</span>
+                    )}
+                    </td>
+                </tr>
+                </tfoot>
+            </table>
+        </p>
       </div>
     </div>
   );
